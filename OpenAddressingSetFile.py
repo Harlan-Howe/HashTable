5d5c3023d4bbe9660d3f283ee517239d9d6fbd90
harlan.howe <harlan.howe@kinkaid.org>
2023-12-04 13:18:49 -0600

    Added stub function for draw_hash_table; importing typing.Tuple and numpy

--- OpenAddressingSetFile.py
+++ OpenAddressingSetFile.py
@@ -1,7 +1,19 @@
+from typing import Tuple
 from AbstractSetFile import BasicAbstractSet, AdvancedAbstractSet
+import numpy as np
 
-
-class OpenAddressingSetFile(BasicAbstractSet):
+class OpenAddressingSet(BasicAbstractSet):
 
     def __init__(self):
         pass
+
+    def draw_hash_table(self, drawing_area: np.ndarray, start_pos: Tuple[int, int]) -> None:
+        """
+        draws all the ColorShapes in this set on the drawing_area in a way that illustrates how the hash table is
+        organized.
+        :param drawing_area: the surface on which to draw.
+        :param start_pos: the upper-left corner of this hash table's drawing.
+        :return:  None
+        """
+        pass
+        # Hint: look at main's demo_drawing() for inspiration....
